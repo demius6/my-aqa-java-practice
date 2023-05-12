41b628283734c18120bdfb0c0b17daf16e825623
“DemiuS“ <you@example.com>
2023-05-12 22:50:57 +0300

    upd

--- src/test/java/junit/FirstJUnitTest.java
+++ src/test/java/junit/FirstJUnitTest.java
@@ -1,2 +1,11 @@
-package junit;public class FirstJUnitTest {
+package junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstJUnitTest {
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(2 < 3);
+    }
 }
