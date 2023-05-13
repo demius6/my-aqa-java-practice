bec3c95e06a82236f9783d040e52445ef457c48f
“DemiuS“ <you@example.com>
2023-05-13 22:16:48 +0300

    complited homework #1

---
+++ src/test/java/junit/TextBoxTests.java
@@ -0,0 +1,48 @@
+package junit;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAAll() {
+        Configuration.baseUrl ="https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+    @Test
+    void successFillTest() {
+        open("/automation-practice-form");
+        $(".main-header").shouldHave(text("Practice Form"));
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Yakimovich");
+        $("#userEmail").setValue("alex@alex.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("1231231230");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("March");
+        $(".react-datepicker__year-select").selectOption("1999");
+        $("[aria-label$='Tuesday, March 2nd, 1999']").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("kotik.jpg");
+        $("#currentAddress").setValue("Some address 1");
+        $("#state").scrollTo().click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("Student Name"))
+                .parent().shouldHave(text("Alex Yakimovich"));
+
+    }
+}
